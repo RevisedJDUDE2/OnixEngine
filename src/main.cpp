124d162bf0581c68b053d40f2cd90723423a364b
Jude Catanda <97071945+RevisedJDUDE2@users.noreply.github.com>
2024-06-14 15:15:32 +0800

    for loop makes it unresponsive

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include "Buffers.hpp"
 #include "Shader.hpp"
 
+#define TO_STRING(integer) #integer
+
 //# EXTERNALS
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
@@ -29,14 +31,25 @@ int main(void) {
     0, 1, 2,
     0, 3, 2
   };
-  int count = 10;
-  glm::vec3* Positions = new glm::vec3[count];
-  for (int i = 0; i < count; i++) {
-    float ft = 0.1f;
-    Positions[i].x = ft;
-    Positions[i].y = ft;
-    ft += 0.1f;
-  };
+  //int count = 10;
+  //glm::vec3* Positions = new glm::vec3[count];
+  //for (int i = 0; i < count; i++) {
+  //  float ft = 0.1f;
+  //  Positions[i].x = ft;
+  //  Positions[i].y = ft;
+  //  ft += 0.1f;
+  //};
+  glm::vec3 translations[10];
+  int index = 0;
+  float offset = 0.1f;
+  for(int y = -10; y < 10; y += 2) {
+    for(int x = -10; x < 10; x += 2) {
+        glm::vec3 translation;
+        translation.x = (float)x / 10.0f + offset;
+        translation.y = (float)y / 10.0f + offset;
+        translations[index++] = translation;
+    }
+  } 
   //Onix::Buffer VAO(GL_VERTEX_ARRAY);
   //VAO.Bind();
   //Onix::Buffer VBO(GL_ARRAY_BUFFER, sizeof(Triangles), Triangles, GL_STATIC_DRAW);
@@ -61,9 +74,9 @@ int main(void) {
 
   glGenBuffers(1, &INSTANCED);
   glBindBuffer(GL_ARRAY_BUFFER, INSTANCED);
-  glBufferData(GL_ARRAY_BUFFER, count * sizeof(glm::vec3), &Positions[0], GL_STATIC_DRAW);
-  fprintf(stdout, "Size of instanced: %d", count * 3 * sizeof(float));
-  fflush(stdout);
+  glBufferData(GL_ARRAY_BUFFER, 10 * sizeof(glm::vec3), &translations[0], GL_STATIC_DRAW);
+  //fprintf(stdout, "Size of instanced: %d", 10 * 3 * sizeof(float));
+  //fflush(stdout);
 
   glGenBuffers(1, &EBO);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
@@ -74,17 +87,17 @@ int main(void) {
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertices), (void*)offsetof(Vertices, Position));
   glEnableVertexAttribArray(1);
   glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertices), (void*)offsetof(Vertices, Color));
-  glEnableVertexAttribArray(2);
-  glBindBuffer(GL_ARRAY_BUFFER, INSTANCED);
-  glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-  glVertexAttribDivisor(2, 1);
-  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  //glEnableVertexAttribArray(2);
+  //glBindBuffer(GL_ARRAY_BUFFER, INSTANCED);
+  //glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  //glVertexAttribDivisor(2, 1);
+  //glBindBuffer(GL_ARRAY_BUFFER, 0);
   glBindVertexArray(0);
 
   while (!glfwWindowShouldClose(MainWindow.Get())) {
     glClear(GL_COLOR_BUFFER_BIT);
     if (glfwGetKey(MainWindow.Get(), GLFW_KEY_W)) {
-      Positions[0].x += 0.1f;
+      translations[0].x += 0.1f;
     }
     Program.UseProgram();
     glBindVertexArray(VAO);
